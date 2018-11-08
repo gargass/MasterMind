96887ed8aecae2fa3917158fb04c3ed5eafc369d
gargass <sebastian.gargas92@gmail.com>
2018-11-08 21:22:18 +0100

    init shiny pins

--- App_MasterMind/ui.R
+++ App_MasterMind/ui.R
@@ -18,16 +18,16 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
+       numericInput("n_pins",
+                   "Number of pins:",
                    min = 1,
-                   max = 50,
-                   value = 30)
+                   max = 8,
+                   value = 4)
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+       tableOutput("df")
     )
   )
 ))
